288a986ee1adb71b5435e229bc8e2cdb85fcb248
grupo03marvel <grupo03marvel@gmail.com>
2019-11-12 18:10:51 -0300

    alteracoes filipe

---
+++ app/src/main/java/com/example/meuuniversomarvel/view/interfaces/OnClick.java
@@ -0,0 +1,4 @@
+package com.example.meuuniversomarvel.view.interfaces;
+
+public interface OnClick {
+}
---
+++ app/src/main/java/com/example/meuuniversomarvel/viewmodel/AppUtils.java
@@ -0,0 +1,4 @@
+package com.example.meuuniversomarvel.viewmodel;
+
+public class AppUtils {
+}
